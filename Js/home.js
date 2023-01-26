67ba24b515ccf81157f5b96eea88f52144d3e5b9
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2023-01-26 16:05:48 +0200

    Update home.js

--- Js/home.js
+++ Js/home.js
@@ -2,3 +2,8 @@ function dark() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+function light() {
+  var element = document.body;
+  element.classList.toggle("light-mode");
+}
